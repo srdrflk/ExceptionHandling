d34cc69756018c4ff35857fbd2822cdb507f1f1c
srdrflk <55311271+srdrflk@users.noreply.github.com>
2024-12-16 18:10:08 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            char WriteFirstLetter(string input)
+            {
+                if (input.Trim() == string.Empty)
+                    throw new ArgumentException(message: "Input string cannot be empty.", input);
+
+                if (input == null)
+                    throw new ArgumentNullException(nameof(input), message: "Input string cannot be null.");
+
+                char[] characters = input.ToCharArray();
+                return characters.First();
+
+            }
+
+            Console.WriteLine(WriteFirstLetter(" "));
+        }
+
+
+    }
+}
---
+++ Task2/NumberParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            if (stringValue == null)
+                throw new ArgumentNullException("Input string cannot be null.");
+
+            stringValue = stringValue.Trim();
+
+            if (stringValue.Length == 0)
+                throw new FormatException("Input string is empty or only contains whitespace.");
+
+            bool isNegative = stringValue[0] == '-';
+            int startIndex = (stringValue[0] == '+' || stringValue[0] == '-') ? 1 : 0;
+
+
+            int result = 0;
+            for (int i = startIndex; i < stringValue.Length; i++)
+            {
+                char chr = stringValue[i];
+                if (chr < '0' || chr > '9')
+                    throw new FormatException($"Input string contains invalid character '{chr}'.");
+
+                int digit = chr - '0';
+
+                // Overflow check
+                int tempResult = result * 10 + digit;
+                if ((isNegative && -tempResult > 0) || (!isNegative && tempResult < 0))
+                {
+                    throw new OverflowException("Numeric value is out of Int32 range.");
+                }
+                else
+                {
+                    result = tempResult;
+                }
+            }
+
+            if (isNegative)
+            {
+                // When condition matches exactly to the edge case of int.MinValue without overflow exception
+                if (stringValue == "-2147483648")
+                {
+                    return int.MinValue;
+                }
+                else
+                {
+                    return -result;
+                }
+            }
+
+            return result;
+        }
+    }
+}
